f368fe2fd37092de521920a3d0a2dd20fcaf8876
25730152 <25730152@ms.uit.edu.vn>
2026-05-06 12:51:18 +0700

    drawMainBoard

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ char board[H][W];
 int x, y, b;
 
 // 7 tetrominoes
-char blocks[7][4][4] = {
+char blocks[7][4][4] =
+{
     // I
     {
         {' ',' ',' ',' '},
@@ -64,8 +65,8 @@ char blocks[7][4][4] = {
         {' ',' ',' ',' '}
     }
 };
-
-void initBoard() {
+void initBoard()
+{
     for (int i = 0; i < H; i++)
         for (int j = 0; j < W; j++)
             if (i == 0 || i == H - 1 || j == 0 || j == W - 1)
@@ -74,19 +75,93 @@ void initBoard() {
                 board[i][j] = ' ';
 }
 
-void draw() {
-    system("cls");
-    for (int i = 0; i < H; i++) {
+// ====================== COLOR ======================
+
+void setColor(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+int getColor(char c)
+{
+    if (c == 'I') return 11; // cyan
+    if (c == 'O') return 14; // yellow
+    if (c == 'T') return 13; // purple
+    if (c == 'S') return 10; // green
+    if (c == 'Z') return 12; // red
+    if (c == 'J') return 9;  // blue
+    if (c == 'L') return 6;  // orange
+
+    return 7;
+}
+
+// ====================== LOGIC ======================
+// ====================== DRAW ======================
+
+void drawCell(char c)
+{
+    if (c == '#')
+    {
+        setColor(8);
+        cout << "##";
+    }
+    else if (c == ' ')
+    {
+        cout << "  ";
+    }
+    else
+    {
+        setColor(getColor(c));
+        cout << "[]";
+    }
+
+    setColor(7);
+}
+void drawMainBoard()
+{
+    for (int i = 0; i < H; i++)
+    {
         for (int j = 0; j < W; j++)
-            cout << board[i][j];
+        {
+            drawCell(board[i][j]);
+        }
+
         cout << endl;
     }
 }
 
-bool canMove(int dx, int dy) {
+
+void draw()
+{
+    system("cls");
+
+    drawMainBoard();
+
+    setColor(15);
+
+    COORD pos = {35, 15};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+    cout << "A : LEFT";
+
+    pos = {35, 16};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+    cout << "D : RIGHT";
+
+    pos = {35, 17};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+    cout << "X : DOWN";
+
+    pos = {35, 18};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+    cout << "Q : QUIT";
+}
+
+bool canMove(int dx, int dy)
+{
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
-            if (blocks[b][i][j] != ' ') {
+            if (blocks[b][i][j] != ' ')
+            {
                 int xt = x + j + dx;
                 int yt = y + i + dy;
 
@@ -99,14 +174,16 @@ bool canMove(int dx, int dy) {
     return true;
 }
 
-void placeBlock() {
+void placeBlock()
+{
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
             if (blocks[b][i][j] != ' ')
                 board[y + i][x + j] = blocks[b][i][j];
 }
 
-void clearBlock() {
+void clearBlock()
+{
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
             if (blocks[b][i][j] != ' ')
@@ -114,7 +191,8 @@ void clearBlock() {
 }
 
 // Rotate 90 degree
-void rotateBlock() {
+void rotateBlock()
+{
     char temp[4][4];
 
     for (int i = 0; i < 4; i++)
@@ -124,7 +202,8 @@ void rotateBlock() {
     // check valid rotation
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
-            if (temp[i][j] != ' ') {
+            if (temp[i][j] != ' ')
+            {
                 int xt = x + j;
                 int yt = y + i;
 
@@ -141,7 +220,8 @@ void rotateBlock() {
             blocks[b][i][j] = temp[i][j];
 }
 
-int main() {
+int main()
+{
     srand(time(0));
 
     initBoard();
@@ -150,11 +230,13 @@ int main() {
     y = 0;
     b = rand() % 7;
 
-    while (true) {
+    while (true)
+    {
         clearBlock();
 
         // input
-        if (kbhit()) {
+        if (kbhit())
+        {
             char c = getch();
 
             if (c == 'a' && canMove(-1, 0)) x--;
@@ -165,9 +247,12 @@ int main() {
         }
 
         // auto fall
-        if (canMove(0, 1)) {
+        if (canMove(0, 1))
+        {
             y++;
-        } else {
+        }
+        else
+        {
             placeBlock();
 
             // new block
@@ -176,7 +261,8 @@ int main() {
             b = rand() % 7;
 
             // game over check
-            if (!canMove(0, 0)) {
+            if (!canMove(0, 0))
+            {
                 draw();
                 cout << "\nGAME OVER\n";
                 break;
